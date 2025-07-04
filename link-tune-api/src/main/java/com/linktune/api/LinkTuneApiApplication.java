61fb193a24022493b71d2f08e197fbf38c856e65
muhammetsaglamm <muhammetsaglam325@gmail.com>
2025-07-04 11:32:44 +0300

    feat: Initial project structure from Spring Initializr

---
+++ link-tune-api/src/main/java/com/linktune/api/LinkTuneApiApplication.java
@@ -0,0 +1,13 @@
+package com.linktune.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkTuneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkTuneApiApplication.class, args);
+	}
+
+}
