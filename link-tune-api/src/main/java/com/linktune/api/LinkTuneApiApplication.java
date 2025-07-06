633082f20b9bd14a433bab68ee54afda2b8a2588
muhammetsaglamm <muhammetsaglam325@gmail.com>
2025-07-06 19:19:35 +0300

    fix: Correct Dockerfile and properties for compose

--- link-tune-api/src/main/java/com/linktune/api/LinkTuneApiApplication.java
+++ link-tune-api/src/main/java/com/linktune/api/LinkTuneApiApplication.java
@@ -2,7 +2,12 @@ package com.linktune.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories; // YENİ IMPORT
 
+@EnableCaching
+// YENİ ANNOTATION: Spring'e Repository'lerin nerede olduğunu söylüyoruz.
+@EnableJpaRepositories(basePackages = "com.linktune.api.Repository")
 @SpringBootApplication
 public class LinkTuneApiApplication {
 
